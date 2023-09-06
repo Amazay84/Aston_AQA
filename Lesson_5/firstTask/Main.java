9245972858a55b86092988c837ccd48490ef38ed
mazay <andymazay84gmail.com>
2023-09-06 19:40:06 +0300

    Lesson_5 Animals

--- Lesson_5/firstTask/Main.java
+++ Lesson_5/firstTask/Main.java
@@ -1,33 +1,11 @@
 package firstTask;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Dog bobik = new Dog("Bobik");
-        Cat murzik = new Cat("Murzik");
-        Dog sharik = new Dog("Sharik");
-        Cat vaska = new Cat("Vaska");
-        Dog polkan = new Dog("Polkan");
-        Cat red = new Cat("Red");
-        Dog jerry = new Dog("Jerry");
-        Cat bloom = new Cat("bloom");
-
-
-        bobik.run(499);
-        bobik.swim(9);
-        murzik.run(199);
-        murzik.swim(11);
-        sharik.run(501);
-        sharik.swim(11);
-        vaska.run(201);
-        vaska.swim(0);
-        polkan.run(-1);
-        polkan.swim(-1);
-        red.run(-1);
-        red.swim(-1);
-        jerry.run(500);
-        jerry.swim(10);
-        bloom.run(200);
-        bloom.swim(9);
+        Dog[] dogs = {new Dog("Bobik"), new Dog("Sharik"), new Dog("Polkan"), new Dog("Jerry")};
+        Cat[] cats = {new Cat("Murzik"), new Cat("Vaska"), new Cat("Red"), new Cat("bloom")};
 
         System.out.println();
 
---
+++ Lesson_5/firstTask/PlateOfFood.java
@@ -0,0 +1,17 @@
+package firstTask;
+
+public class PlateOfFood {
+    private int capacity;
+
+    public PlateOfFood(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
