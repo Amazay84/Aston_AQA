6b24d862e348ee59987a7928ae6b48dadd2d35fd
mazay <andymazay84gmail.com>
2023-09-08 18:50:17 +0300

    Lesson_6 initial

---
+++ Lesson_6/AppData.java
@@ -0,0 +1,10 @@
+public class AppData {
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+}
---
+++ Lesson_6/Main.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("." + System.lineSeparator() + "file.csv");
+        if (file.exists()) {
+            file.createNewFile();
+        }
+    }
+}
