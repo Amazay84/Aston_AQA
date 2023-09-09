5eb19bd01600c8789d0e054be91e4e733f5225c3
mazay <andymazay84gmail.com>
2023-09-09 18:43:50 +0300

    Lesson_6 ready

--- Lesson_6/AppData.java
+++ Lesson_6/AppData.java
@@ -1,10 +1,100 @@
+import com.sun.net.httpserver.Headers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class AppData {
-    private String[] header;
-    private int[][] data;
+    private static String[] header;
+    private static int[][] data;
 
     public AppData(String[] header, int[][] data) {
-        this.header = header;
-        this.data = data;
+        AppData.header = header;
+        AppData.data = data;
     }
 
+    public static String[] getHeader() {
+        return header;
+    }
+
+    public static int[][] getData() {
+        return data;
+    }
+
+    public static AppData getAppData(File file) throws IOException {
+        if (file.exists()) {
+            file.createNewFile();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String headersString = "";
+        String dataString = "";
+        byte[] buffer = new byte[30];
+        int count;
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            while ((count = fis.read(buffer)) > 0) {
+                for (int i = 0; i < count; i++) {
+                    sb.append((char) buffer[i]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        headersString = sb.substring(0, sb.indexOf(System.lineSeparator()));
+        dataString = sb.substring(sb.indexOf(System.lineSeparator()));
+        String[] header = headersString.trim().split(";,");
+
+        String[] dataTemp = dataString.trim().split(System.lineSeparator());
+        int[][] data = new int[dataTemp.length][header.length];
+
+        for (int i = 0; i < data.length; i++) {
+            String[] temp = dataTemp[i].split(";,");
+            int[] intTemp = new int[temp.length];
+            for (int j = 0; j < data[i].length; j++) {
+                intTemp[j] = Integer.parseInt(temp[j]);
+            }
+            data[i] = Arrays.copyOf(intTemp, intTemp.length);
+        }
+        return new AppData(header, data);
+    }
+
+    public static String saveAppData(AppData appData) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < header.length; i++) {
+            sb.append(header[i]);
+            if (i < header.length - 1) {
+                sb.append(";,");
+            } else {
+                sb.append(System.lineSeparator());
+            }
+        }
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                sb.append(data[i][j]);
+                if (j < data[j].length - 1) {
+                    sb.append(";,");
+                } else {
+                    sb.append(System.lineSeparator());
+                }
+            }
+        }
+        byte[] output = sb.toString().getBytes();
+        try (FileOutputStream fos = new FileOutputStream("c:\\Users\\Администратор\\Desktop\\file.csv")) {
+            fos.write(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+    @Override
+    public String toString() {
+        return "AppData:\n" +
+                "header = " + Arrays.deepToString(header) +
+                "\n" +
+                "data = " + Arrays.deepToString(data);
+    }
 }
--- Lesson_6/Main.java
+++ Lesson_6/Main.java
@@ -1,11 +1,16 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("." + System.lineSeparator() + "file.csv");
+        File file = new File("D:\\programming\\Aston_AQA\\Lesson_6\\file.csv");
         if (file.exists()) {
             file.createNewFile();
         }
+        System.out.println(AppData.getAppData(file));
+        AppData appData = new AppData(AppData.getHeader(), AppData.getData());
+        System.out.println(AppData.saveAppData(appData));
     }
 }
