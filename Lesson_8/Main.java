fb5a72363648ae98cf12be9ecd10c15ce28eb443
mazay <andymazay84gmail.com>
2023-09-15 12:30:35 +0300

    Lesson_8 first

---
+++ Lesson_8/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"Hello", "Java", "Hello", "World", "Goodbye", "Welcome", "Java", "Friday",
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        List<String> arrList = Arrays.asList(arr);
+        Map<String, Integer> listMap = new HashMap<>();
+
+        arrList.forEach(k -> listMap.put(k, listMap.getOrDefault(k, 0) + 1));
+
+//        for (String s : arrList) {
+//            if (listMap.containsKey(s)) {
+//                listMap.put(s, listMap.get(s) + 1);
+//            } else {
+//                listMap.put(s, 1);
+//            }
+//        }
+
+        listMap.entrySet().
+                forEach(entry ->
+                        System.out.println("Word: " + entry.getKey() +
+                                " | Repeat count: " + entry.getValue()));
+    }
+}
