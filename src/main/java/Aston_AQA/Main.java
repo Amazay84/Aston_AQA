f90fec2944fe79263029e0225aabe0e1797ff2ee
mazay <andymazay84gmail.com>
2023-10-06 15:09:09 +0300

    StartTest

--- src/main/java/Aston_AQA/Main.java
+++ src/main/java/Aston_AQA/Main.java
@@ -15,25 +15,25 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        File txt = new File("src/main/resources/data/chromeTest.txt");
-        List<String> attr = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(txt))) {
-            attr = br.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        attr.stream().forEach(System.out::println);
-//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver/chromedriver.exe");
-//        WebDriver driver;
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
-//        driver.get("https://www.mts.by/");
-//
-//        WebElement element = driver.findElement(By.xpath("//section/div/div[2]/ul/li[6]/img"));
-//        String result = element.getAttribute("src");
-//
-//        System.out.println(result);
-//        driver.quit();
+//        File txt = new File("src/main/resources/data/chromeTest.txt");
+//        List<String> attr = new ArrayList<>();
+//        try (BufferedReader br = new BufferedReader(new FileReader(txt))) {
+//            attr = br.lines().collect(Collectors.toList());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        attr.stream().forEach(System.out::println);
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver/chromedriver.exe");
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        driver.get("https://www.mts.by/");
+
+        WebElement element = driver.findElement(By.xpath("//section/div/div[2]/ul/li[6]/img"));
+        boolean result = element.isDisplayed();
+
+        System.out.println(result);
+        driver.quit();
     }
 }
--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -14,22 +14,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ChromeTest {
     private static WebDriver driver;
-
-    public static String getData(int index) {
-        File txt = new File("src/main/resources/data/chromeTest.txt");
-        List<String> attr = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(txt))) {
-            attr = br.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return attr.get(index);
-    }
 
     @BeforeAll
     static void setupAll() {
@@ -63,29 +51,18 @@ public class ChromeTest {
     @Test
     void findLogoPaySys() {
         WebElement element1 = driver.findElement(By.xpath("//section/div/div[2]/ul/li[1]/img"));
-        String actual1 = element1.getAttribute("src");
-        String expected1 = getData(0);
         WebElement element2 = driver.findElement(By.xpath("//section/div/div[2]/ul/li[2]/img"));
-        String actual2 = element2.getAttribute("src");
-        String expected2 = getData(1);
         WebElement element3 = driver.findElement(By.xpath("//section/div/div[2]/ul/li[3]/img"));
-        String actual3 = element3.getAttribute("src");
-        String expected3 = getData(2);
         WebElement element4 = driver.findElement(By.xpath("//section/div/div[2]/ul/li[4]/img"));
-        String actual4 = element4.getAttribute("src");
-        String expected4 = getData(3);
         WebElement element5 = driver.findElement(By.xpath("//section/div/div[2]/ul/li[5]/img"));
-        String actual5 = element5.getAttribute("src");
-        String expected5 = getData(4);
         WebElement element6 = driver.findElement(By.xpath("//section/div/div[2]/ul/li[6]/img"));
-        String actual6 = element6.getAttribute("src");
-        String expected6 = getData(5);
-        assertAll("Element didn't found",
-                () -> assertEquals(expected1, actual1),
-                () -> assertEquals(expected2, actual2),
-                () -> assertEquals(expected3, actual3),
-                () -> assertEquals(expected4, actual4),
-                () -> assertEquals(expected5, actual5),
-                () -> assertEquals(expected6, actual6));
+        assertAll(() -> assertTrue(element1.isDisplayed()),
+                () -> assertTrue(element2.isDisplayed()),
+                () -> assertTrue(element3.isDisplayed()),
+                () -> assertTrue(element4.isDisplayed()),
+                () -> assertTrue(element5.isDisplayed()),
+                () -> assertTrue(element6.isDisplayed()));
     }
+
+
 }
