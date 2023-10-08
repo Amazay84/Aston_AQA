248e41cde6ec4c50f202958413175d2aff2a3036
mazay <andymazay84gmail.com>
2023-10-08 21:37:55 +0300

    iFrameTest

--- src/main/java/Aston_AQA/Main.java
+++ src/main/java/Aston_AQA/Main.java
@@ -1,21 +1,11 @@
 package Aston_AQA;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import javax.lang.model.element.Element;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
+    private static String labelWithText(String text) {
+        return "//label[text()=".concat("'").concat(text).concat("'").concat("]");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println(Main.labelWithText("Номер карты"));
     }
 }
--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,32 +1,33 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.junit.platform.commons.util.StringUtils;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.xml.xpath.XPath;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
 public class ChromeTest {
     private static WebDriver driver;
 
+    private WebElement elementWithXpath(WebDriver driver, String text) {
+        WebElement element = driver.findElement(By.xpath(text));
+        return element;
+    }
+
+    private String labelWithText(String text) {
+        return "//label[text()=".concat("'").concat(text).concat("'").concat("]");
+    }
+
+    private String buttonWithText(String text) {
+        return "//button[text()=".concat("'").concat(text).concat("'").concat("]");
+    }
+
+//    private WebElement elementWithCss(WebDriver driver, String text) {
+//        WebElement element = driver.findElement(By.cssSelector(text));
+//        return element;
+//    }
     @BeforeAll
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
@@ -38,6 +39,15 @@ public class ChromeTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
         driver.get("https://www.mts.by/");
+        WebElement phone = elementWithXpath(driver, "//*[@id='connection-phone']");
+        WebElement amount = elementWithXpath(driver, "//*[@id='connection-sum']");
+        WebElement email = elementWithXpath(driver, "//*[@id='connection-email']");
+        phone.click();
+        phone.sendKeys("297777777");
+        amount.click();
+        amount.sendKeys("99");
+        email.click();
+        email.sendKeys("test@test.by");
     }
 
     @AfterEach
@@ -46,71 +56,25 @@ public class ChromeTest {
     }
 
     @Test
-    void checkBlockNameTest() {
-        List<WebElement> elements = driver.findElements(By.xpath("//section/div/h2"));
-        if (elements.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        String actual = elements.stream().map(WebElement::getText).collect(Collectors.joining());
-        String expected = "Онлайн пополнение\n".concat("без комиссии");
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findLogoPaySysTest() {
-        WebElement visa = driver.findElement(By.xpath("//section/div/div[2]/ul/li[1]/img"));
-        WebElement verifiedByVisa = driver.findElement(By.xpath("//section/div/div[2]/ul/li[2]/img"));
-        WebElement MasterCard = driver.findElement(By.xpath("//section/div/div[2]/ul/li[3]/img"));
-        WebElement masterCardSecureCode = driver.findElement(
-                By.xpath("//section/div/div[2]/ul/li[4]/img"));
-        WebElement belCard = driver.findElement(By.xpath("//section/div/div[2]/ul/li[5]/img"));
-        WebElement mir = driver.findElement(By.xpath("//section/div/div[2]/ul/li[6]/img"));
-        assertAll(() -> assertTrue(visa.isDisplayed()),
-                () -> assertTrue(verifiedByVisa.isDisplayed()),
-                () -> assertTrue(MasterCard.isDisplayed()),
-                () -> assertTrue(masterCardSecureCode.isDisplayed()),
-                () -> assertTrue(belCard.isDisplayed()),
-                () -> assertTrue(mir.isDisplayed()));
-    }
-
-    @Test
-    void serviceDetailsLincTest() {
-        WebElement aboutService = driver.findElement(By.xpath("//section[@class = 'pay']/div/a"));
-        aboutService.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
-        String title = driver.getTitle();
-        String expected = "Порядок оплаты и безопасность интернет платежей";
-        Assertions.assertEquals(expected, title);
-    }
-
-    @Test
     void continueButtonTest() {
-        WebElement phone = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
-        WebElement amount = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
-        WebElement email = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
-        WebElement nextButton = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
-        phone.click();
-        phone.sendKeys("297777777");
-        amount.click();
-        amount.sendKeys("99");
-        email.click();
-        email.sendKeys("test@test.by");
+        WebElement nextButton = elementWithXpath(driver, buttonWithText("Продолжить"));
         nextButton.click();
-        WebDriver iFrame = driver.switchTo().frame(
-                driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']")));
+        WebDriver iFrame = driver.switchTo().frame(elementWithXpath(driver, "//iframe[@class='bepaid-iframe']"));
         try {
-            Thread.sleep(3000);//не исправлять, тут какя-то магия
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         List<WebElement> elements = iFrame.findElements(
                 By.xpath("//div[@class='header__payment']/p"));
-        elements.stream().forEach(e -> System.out.println(e.getText()));
-        iFrame.findElement(By.xpath("//svg-icon[@class='header__close-icon']")).click();
-//        iFrame.close();
-//        WebElement head = wait.until(ExpectedConditions.presenceOfElementLocated(
-//                By.xpath("/html/head/title[text()='BePaidWidget']")));
-//
+        WebElement iPayButton = elementWithXpath(iFrame, "//app-card-page//button/text()");
+        boolean iInputCardNum = elementWithXpath(iFrame, labelWithText("Номер карты")).isDisplayed();
+
+        assertAll(() -> assertEquals("Оплатить 99.00 BYN", iPayButton.getText()),
+                () -> assertTrue(iInputCardNum));
+
+
+//        iFrame.findElement(By.xpath("//svg-icon[@class='header__close-icon']")).click();
 //        Assertions.assertEquals("BePaidWidget", head.getText());
     }
 }
