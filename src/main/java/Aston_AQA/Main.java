a30385b3a8fc2af37f4c29c9eb2a93fdd160792b
mazay <andymazay84gmail.com>
2023-10-08 15:44:38 +0300

    StartTest

--- src/main/java/Aston_AQA/Main.java
+++ src/main/java/Aston_AQA/Main.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.lang.model.element.Element;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,25 +16,6 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-//        File txt = new File("src/main/resources/data/chromeTest.txt");
-//        List<String> attr = new ArrayList<>();
-//        try (BufferedReader br = new BufferedReader(new FileReader(txt))) {
-//            attr = br.lines().collect(Collectors.toList());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        attr.stream().forEach(System.out::println);
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver/chromedriver.exe");
-        WebDriver driver;
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
-        driver.get("https://www.mts.by/");
-
-        WebElement element = driver.findElement(By.xpath("//section/div/div[2]/ul/li[6]/img"));
-        boolean result = element.isDisplayed();
-
-        System.out.println(result);
-        driver.quit();
+        System.out.println("Java");
     }
 }
